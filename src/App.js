2deaee7641804fb90981b736bbbee2f217312dbf
Annalise Murphy <nali247@gmail.com>
2020-04-30 16:55:54 -0700

    drop down added

--- src/App.js
+++ src/App.js
@@ -3,30 +3,12 @@ import request from 'superagent';
 import './App.css';
 // import QuoteList from './QuoteList.js';
 
-//need to input the actual pokemon data
-const data = [
-  {
-    character: 'some character 1',
-    quote: 'some quote 1',
-    img: 'some img 1'
-  },
-  {
-    character: 'some character 2',
-    quote: 'some quote 2',
-    img: 'some img 2'
-  },
-  {
-    character: 'some character3',
-    quote: 'some quote 3',
-    img: 'some img 3'
-  },
-]
-
 export default class App extends Component {
   state = {
     searchQuery: null,
     defenseQuery: null,
-    data: data,
+    data: [{}],
+    selected: ''
   }
 
   handleChange = (event) => {
@@ -36,31 +18,46 @@ export default class App extends Component {
   }
 
   handleClick = async () => {
+    // const query = this.state.query;
+    
     const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
     console.log(fetchedData.body.results);
-    this.setState({ data: fetchedData })
+    this.setState({ data: fetchedData.body.results })
   }
 
-  //drop down handler
-  // sortBy = (e) => {
-  //   // const value = e.target.value;
-  //   const fetchedData = type.searchQuery(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`)
-
-  //   console.log(fetchedData.body.type);
-  //   this.setState({ data: fetchedData })
-  // }
-
   //type.searchQuery > work with this based off of type
-  //dropdown into handleChange with added other urls
 
 
   render() {
+
     return (
+      <main>
       <div>
         <input onChange={this.handleChange} />
+
         <button onClick={this.handleClick}>Search</button>
-        {/* <QuoteList quotes={this.state.data} /> */}
+        <select onChange={this.handleOptionChange}>
+          <option value="" >All</option>
+          <option value="normal" >Normal</option>
+          <option value="fire" >Fire</option>
+          <option value="water" >Water</option>
+          <option value="grass" >Grass</option>
+          <option value="flying" >Flying</option>
+          <option value="bug" >Bug</option>
+          <option value="poison" >Poison</option>
+          <option value="dark" >Dark</option>
+          <option value="electric" >Electric</option>
+          <option value="fairy" >Fairy</option>
+          <option value="fighting" >Fighting</option>
+          <option value="ghost" >Ghost</option>
+          <option value="ground" >Ground</option>
+          <option value="ice" >Ice</option>
+          <option value="psychic" >Psychic</option>
+          <option value="rock" >Rock</option>
+          <option value="steel" >Steel</option>\
+        </select>
       </div>
+      </main>
     )
   }
 }
--- src/Data.js
+++
@@ -1,4 +0,0 @@
-// const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
-
-
-//munge my data - know the shape of my data (api call)
