86f0ebcdfab5b1dd650097b7f6c0e6193b3e86d2
Annalise Murphy <nali247@gmail.com>
2020-04-30 14:49:10 -0700

    data.js needs to be placed

--- src/Data.js
+++ src/Data.js
@@ -1,1 +1,4 @@
 // const fetchedData = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?pokemon=${this.state.searchQuery}`);
+
+
+//munge my data - know the shape of my data (api call)
