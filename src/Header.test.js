a214641ef2de35b27403c94ab4b29f39b508aa43
Annalise Murphy <nali247@gmail.com>
2020-05-03 19:07:24 -0700

    testing more testing...

--- src/Header.test.js
+++ src/Header.test.js
@@ -2,6 +2,16 @@
 // import { shallow } from 'enzyme';
 // import Header from './Header.js';
 
+// test[`renders correctly when there is a greeting 1`] = `
+// <div>
+//     <h1>
+//     Welcome!
+//     !
+//     </h1>
+// </div>`;
+
+// exports[`renders correctly when there is not a greeting 1`] = `<div />`;
+
 // exports[`renders the drop down for type 1`] = `
 // <header>
 //     <h1>
